685926aa73876a38b54bd934803ba56ebed9fd4b
Zoe-WangZh <57276972+Zoe-WangZh@users.noreply.github.com>
2019-11-03 09:50:33 +0800

    Add files via upload

---
+++ EEG-P300Speller/Ensamble SVM/biaccuracy.m
@@ -0,0 +1,15 @@
+    
+%% ??¡¤????¡¤???¨¤??????¡À?
+accurate=0;
+for i=1:15300
+   if output_final(i)==biclass_labelset(i)
+       accurate=accurate+1;
+   end
+end
+accurate/15300
+% for i=1:15300
+%    if output(i)==biclass_labelset(i)
+%        accurate=accurate+1;
+%    end
+% end
+% accurate/15300
